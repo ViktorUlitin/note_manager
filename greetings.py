e4c95292244c3dbb7743cd6086141709185dcf68
ViktorUlitin <viktor.ulitin90@yandex.ru>
2025-01-29 22:55:52 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+user_name = "Виктор"
+title = "Judo"
+content = "отработка_борьбы_в_партере"
+status = "Запланировано на следующую тренировку"
+create_date = "29.01.2025"
+issue_date =  "31.01.2025"
+print (user_name)
+print (title)
+print (content)
+print (status)
+print (create_date)
+print (issue_date)
